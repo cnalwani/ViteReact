d254fb79f32a0a1d09d17fa05b2df39aab8f9e79
Chirag Alwani <chiragalwani123@gmail.com>
2023-04-01 14:18:15 -0400

    added Handler Event

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,32 @@
+import { MouseEvent } from "react";
+
+//import { Fragment } from "react";
+
 function ListGroup() {
+  let items = ["New York", "San Francisco", "London", "Tokyo", "Paris"];
+
+  // items = [];
+  const handlerClick = (event: MouseEvent) => console.log(event);
+
+  if (items.length == 0)
+    return (
+      <>
+        <h1>List</h1>
+        <p>No item Found</p>
+      </>
+    );
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handlerClick}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
-
 export default ListGroup;
