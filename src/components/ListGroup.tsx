0e3398be3c906e0c2f6a8fde251290634f3a1604
Chirag Alwani <chiragalwani123@gmail.com>
2023-04-05 18:26:19 -0400

    added eventHandler and different stages

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,27 +1,27 @@
-import { MouseEvent } from "react";
-
-//import { Fragment } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "London", "Tokyo", "Paris"];
 
-  // items = [];
-  const handlerClick = (event: MouseEvent) => console.log(event);
-
-  if (items.length == 0)
-    return (
-      <>
-        <h1>List</h1>
-        <p>No item Found</p>
-      </>
-    );
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handlerClick}>
+          <li
+            className={
+              selectedIndex == index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
