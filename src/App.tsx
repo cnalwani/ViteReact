9cbf354378e88e0697c21905be1de29aa2de4795
Chirag Alwani <chiragalwani123@gmail.com>
2023-04-12 20:11:05 -0400

    building button components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["New York", "San Francisco", "London", "Tokyo", "Paris"];
+
   return (
     <div>
-      {" "}
-      <ListGroup />
+      <ListGroup items={items} heading={"Cities"} />
     </div>
   );
 }
